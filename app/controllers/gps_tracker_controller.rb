12ff8dca1f84970c8e1ed778e53de908c0dc245a
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2013-06-11 12:48:00 +0530

    Added apk to the public resources and changed the imei creation

--- app/controllers/gps_tracker_controller.rb
+++ app/controllers/gps_tracker_controller.rb
@@ -2,7 +2,7 @@ class GpsTrackerController < ApplicationController
   respond_to :json
 
   def create
-    tracker = Tracker.find_by_imei params[:imei]
+    tracker = Tracker.find_or_create_by_imei params[:imei]
     gps_tracker = GpsTracker.new params.slice(:imei, :latitude, :longitude)
     gps_tracker.tracker = tracker
     gps_tracker.save!
