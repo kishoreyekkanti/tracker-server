d63a8ca24dbcc7428c23521d0e9874f3e57156e8
Yekkanti Kishore Kumar <kishoreyekkanti@gmail.com>
2013-06-15 00:45:04 +0530

    Removed vehicle number drop down and inlined search button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,13 +6,10 @@ class HomeController < ApplicationController
   end
 
   def search
-    start_date_time = params["search"]["start_date_time"].to_time
-    end_date_time = params["search"]["end_date_time"].to_time
-    vehicle_number = params["search"]["vehicle_number"]
-    @tracks = Tracker.joins(:gps_trackers).includes(:gps_trackers).where("gps_trackers.created_at >= ? AND gps_trackers.created_at <=? AND truck_registration_number = ?", start_date_time, end_date_time, vehicle_number)
+    @start_date_time = params["search"]["start_date_time"]
+    @end_date_time = params["search"]["end_date_time"]
+    @tracks = Tracker.search(@start_date_time.to_time, @end_date_time.to_time)
     @tracks = @tracks.to_json(include: :gps_trackers).html_safe
-    #@tracks = @tracks.to_json.html_safe
-    @vehicle_numbers = Tracker.pluck(:truck_registration_number)
     render "map"
   end
 end
--- app/models/tracker.rb
+++ app/models/tracker.rb
@@ -1,5 +1,9 @@
 class Tracker < ActiveRecord::Base
   attr_accessible :driver_name, :mobile_number, :imei, :truck_registration_number
-  has_many :gps_trackers, :order => 'created_at DESC' ,:limit => 1
+  has_many :gps_trackers, :order => 'created_at DESC', :limit => 1
 
+  def self.search(start_date_time, end_date_time)
+    joins(:gps_trackers).includes(:gps_trackers)
+    .where('gps_trackers.created_at >= ? AND gps_trackers.created_at <=?', start_date_time, end_date_time)
+  end
 end
